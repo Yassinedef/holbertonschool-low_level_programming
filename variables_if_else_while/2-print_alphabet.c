86e5dbf431438de92abe3ab07ee6bb63099a0052
Yassinedef <yassinedef@gmail.com>
2025-02-21 13:11:34 +0000

    new syntaxe

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,11 @@
  */
 int main(void)
 {
-int n;
-int i;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-for (i ='a';i<='z'; i++)
-{
-putchar(i);
-}
-putchar('\n');
-return(0)
+    int i;
+    srand(time(0));
+    for (i = 'a'; i <= 'z'; i++) {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
 }
