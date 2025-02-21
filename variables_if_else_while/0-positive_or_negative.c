7ee9d3a2f39de4f0b939dfcc25d961ade9916c4f
Yassinedef <yassinedef@gmail.com>
2025-02-21 09:49:08 +0000

    charactere of comment

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  main - attribue un nombre aleatoire a la variable a chaque fois qu'elle est execute
+ *  main - attribue un nombre aleatoire a la variable a chaque fois
+ * qu'elle est execute
  *  retour : toujours 0
  */
 int main(void)
