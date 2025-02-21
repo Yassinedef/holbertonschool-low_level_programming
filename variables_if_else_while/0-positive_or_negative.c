3ff498f711c7f0867acb09c7d2e2e793e15dca9e
Yassinedef <yassinedef@gmail.com>
2025-02-21 09:30:37 +0000

    delete the return

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	return (0);
-}
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n < 0)
+	if (n < 0)
 {
 printf("%d is negative\n", n);
 }else if (n == 0)
