9300da129eecff80cf6921b0751a0b620e533b4f
Yassinedef <yassinedef@gmail.com>
2025-02-21 12:47:05 +0000

    add return

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - headers goes there 
+ *Return:O
+ */
 int main(void)
 {
 int n;
 int i;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-for (i='a';i < 'z'; i++)
+for (i ='a';i <='z'; i++)
 putchar(i);
 putchar('\n');
+return(0)
 }
